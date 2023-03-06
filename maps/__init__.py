0fb987f142b47b89b77a2679540bf1cc27c71bb5
Fionnuala <fionnuala.mcaoc@gmail.com>
2023-03-06 03:26:56 +0000

    added code for zachs area to updated code

--- maps/__init__.py
+++ maps/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["main_room", "starting_room", "cave_outside", "bedroom", "blacksmith","cave_inside","dojo_outside", "dojo","enemy_house","forest","kitchen","living_room"]
+__all__ = ["main_room", "starting_room", "cave_outside", "bedroom", "blacksmith","cave_inside","dojo_outside", "dojo","enemy_house","forest","kitchen","living_room","dungeon","forest_hideout","lonely_house"]
