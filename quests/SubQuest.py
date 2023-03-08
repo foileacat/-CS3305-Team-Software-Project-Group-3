dafc2e3ee34b7ccf5ab03a8b137721222a988c8f
Fionnuala <fionnuala.mcaoc@gmail.com>
2023-03-08 22:05:13 +0000

    added subquest class

---
+++ quests/SubQuest.py
@@ -0,0 +1,31 @@
+from constants import *
+
+
+class SubquestQuest():
+    def __init__(self,name,description="Nothing here!"):
+        self.name = name
+        self.description = ""
+        self.state = "inactive"
+        self.complete = False
+
+    def activate(self):
+        self.state = "active"
+
+    def complete(self):
+        self.state = "complete"
+        self.complete = True
+    
+    def make_done(self):
+        self.state = "done"
+
+    def is_active(self):
+        if self.state == "active":
+            return True
+        else:
+            return False
+        
+    def is_completed(self):
+        if self.state == "complete":
+            return True
+        else:
+            return False
